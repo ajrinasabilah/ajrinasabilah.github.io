4e5b2043a5fe4171b31b0dd468f55e1f7eca13e8
ajrinasabilah <sabilahajrin@gmail.com>
2023-01-30 06:45:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const txtElement =['Web Developer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+
+})();
+
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+});
